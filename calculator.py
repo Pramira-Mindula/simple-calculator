d7bb0338b590d29d4c02fb0ed2f6f7594a1c884c
Pramira Herath <pramira.herath@haycarb.com>
2026-02-26 11:33:35 +0530

    more operations

--- calculator.py
+++ calculator.py
@@ -1,4 +1,30 @@
+import math
+
+history = []
+
 while True:
+
+    op = input("Enter an Operation (+ , - , x , / , ^ , history , clear , exit) : ")
+
+    if (op.lower()) == "exit":
+        print()
+        break
+
+    elif (op.lower()) == "history":
+        if (len(history) == 0):
+            print("No history yet\n")
+        else:
+            print("\nCalculator History")
+            for item in history:
+                print(item)
+            print()
+        continue
+
+    elif op.lower() == "clear":
+        history.clear()
+        print("History Cleared\n")
+        continue
+
 
     try:
         num1 = float(input("Enter Number 1 : "))
@@ -7,31 +33,42 @@ while True:
         print("Enter valid Numbers!\n")
         continue
 
-    op = input("Enter an Operation (+ , - , x , / , ^) : ")
 
     if op == "+":
-        print(f"Result = {num1 + num2}")
+        result = (num1 + num2)
+        history.append(f"{num1} + {num2} = {result}")
+        print(f"Result = {result}")
 
     elif op == "-":
-        print(f"Result = {num1 - num2}")
+        result = (num1 - num2)
+        history.append(f"{num1} - {num2} = {result}")
+        print(f"Result = {result}")
 
     elif op.lower() == "x":
-        print(f"Result = {num1 * num2}")
+        result = (num1 * num2)
+        history.append(f"{num1} x {num2} = {result}")
+        print(f"Result = {result}")
 
     elif op == "/":
         if (num2 != 0):
-            print(f"Result = {num1 / num2}")
+            result = (num1 / num2)
+            history.append(f"{num1} / {num2} = {result}")
+            print(f"Result = {result}")
         else:
             print("Cannot Divide by Zero")
 
     elif op == "^":
-        print(f"Resul = {num1 ** num2}")
+        result = (num1 ** num2)
+        history.append(f"{num1} ^ {num2} = {result}")
+        print(f"Result = {result}")
 
     else:
         print("Invalid Operator\n")
         continue
 
+
     again = input("Don another? (y/n) :")
+    print()
 
     if again.lower() == "n" or again == "":
         break
