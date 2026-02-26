40ba7abf56d5fae3ca120c55ffddd4783a09d717
Pramira Herath <pramira.herath@haycarb.com>
2026-02-26 11:42:38 +0530

    converted to OOP

--- calculator.py
+++ calculator.py
@@ -1,74 +1,90 @@
 import math
 
-history = []
+class Calculator:
 
-while True:
+    def __init__(self):
+        self.history = []
 
-    op = input("Enter an Operation (+ , - , x , / , ^ , history , clear , exit) : ")
-
-    if (op.lower()) == "exit":
-        print()
-        break
-
-    elif (op.lower()) == "history":
-        if (len(history) == 0):
+    def show_history(self):
+        if len(self.history) == 0:
             print("No history yet\n")
         else:
             print("\nCalculator History")
-            for item in history:
+            for item in self.history:
                 print(item)
             print()
-        continue
 
-    elif op.lower() == "clear":
-        history.clear()
+    def clear_history(self):
+        self.history.clear()
         print("History Cleared\n")
-        continue
 
+    def calculate(self, op, num1, num2):
 
-    try:
-        num1 = float(input("Enter Number 1 : "))
-        num2 = float(input("Enter Number 2 : "))
-    except:
-        print("Enter valid Numbers!\n")
-        continue
+        if op == "+":
+            result = num1 + num2
+            self.history.append(f"{num1} + {num2} = {result}")
+            return result
 
+        elif op == "-":
+            result = num1 - num2
+            self.history.append(f"{num1} - {num2} = {result}")
+            return result
 
-    if op == "+":
-        result = (num1 + num2)
-        history.append(f"{num1} + {num2} = {result}")
-        print(f"Result = {result}")
+        elif op.lower() == "x":
+            result = num1 * num2
+            self.history.append(f"{num1} x {num2} = {result}")
+            return result
 
-    elif op == "-":
-        result = (num1 - num2)
-        history.append(f"{num1} - {num2} = {result}")
-        print(f"Result = {result}")
+        elif op == "/":
+            if num2 != 0:
+                result = num1 / num2
+                self.history.append(f"{num1} / {num2} = {result}")
+                return result
+            else:
+                return "Cannot Divide by Zero"
 
-    elif op.lower() == "x":
-        result = (num1 * num2)
-        history.append(f"{num1} x {num2} = {result}")
-        print(f"Result = {result}")
+        elif op == "^":
+            result = num1 ** num2
+            self.history.append(f"{num1} ^ {num2} = {result}")
+            return result
 
-    elif op == "/":
-        if (num2 != 0):
-            result = (num1 / num2)
-            history.append(f"{num1} / {num2} = {result}")
-            print(f"Result = {result}")
         else:
-            print("Cannot Divide by Zero")
+            return "Invalid Operator"
 
-    elif op == "^":
-        result = (num1 ** num2)
-        history.append(f"{num1} ^ {num2} = {result}")
-        print(f"Result = {result}")
+    def run(self):
 
-    else:
-        print("Invalid Operator\n")
-        continue
+        while True:
+
+            op = input("Enter an Operation (+ , - , x , / , ^ , history , clear , exit) : ")
+
+            if op.lower() == "exit":
+                print()
+                break
+
+            elif op.lower() == "history":
+                self.show_history()
+                continue
+
+            elif op.lower() == "clear":
+                self.clear_history()
+                continue
+
+            try:
+                num1 = float(input("Enter Number 1 : "))
+                num2 = float(input("Enter Number 2 : "))
+            except:
+                print("Enter valid Numbers!\n")
+                continue
+
+            result = self.calculate(op, num1, num2)
+            print(f"Result = {result}")
+
+            again = input("Do another? (y/n) :")
+            print()
+
+            if again.lower() == "n" or again == "":
+                break
 
 
-    again = input("Don another? (y/n) :")
-    print()
-
-    if again.lower() == "n" or again == "":
-        break
+calc = Calculator()
+calc.run()
